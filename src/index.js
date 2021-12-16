0ab2b33fc9e1a431947e69304593d284feb22b08
BrownEdgar <nikogosjanedgar@gmail.com>
2021-12-17 00:23:58 +0400

    add lesson

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/Narek/context/App';
+import App from './Components/Edgar/Router6/App';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 // import App from './Components/Georgi/components/axios/App'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 
