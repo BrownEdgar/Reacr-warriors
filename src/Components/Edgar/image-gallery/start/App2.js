f841a68162533af3e566736d43d70fb02e333530
BrownEdgar <nikogosjanedgar@gmail.com>
2021-12-22 10:18:17 +0400

    add image-geallery exampple

---
+++ src/Components/Edgar/image-gallery/start/App2.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import InfiniteScroll from 'react-infinite-scroll-component'
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+export default function App2() {
+
+}
